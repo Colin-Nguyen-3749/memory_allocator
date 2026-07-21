4bdf2c0eb97527883edf8d6d863723c3fd422337
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-21 14:25:55 -0400

    Create header struct and wrap it in a union

--- main.c
+++ main.c
@@ -17,5 +17,34 @@ void *malloc(size_t size) {
         return NULL;
     };
 
+    // If success, n bytes (n = size) are allocated on the heap
     return block;
 }
+
+//==========================
+// header_t
+// This struct tracks the size of a block of memory
+// and whether or not it is free. We need size because 
+// when we have to free the memory pointed to by ptr, we 
+// need to know the size of what we're freeing.
+// Heap memory is contiguous, and we can only release memory 
+// that's at the end of the heap. However, freeing and releasing
+// memory are two different things. Freeing memory doesn't release
+// it back to the OS, but rather it's just marked as free and can 
+// be reused in a later malloc() call. 
+//============================
+
+typedef char ALIGN[16];
+
+union header {
+    struct {
+        size_t size;
+        unsigned is_free;
+
+        // Use a linked list to keep track of the memory
+        struct header_t *next;
+    } s;
+    ALIGN stub;
+};
+
+typedef union header header_t;
